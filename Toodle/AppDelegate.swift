bc32acf6eb670b7e3799639acc4a84e9f6107f0a
Ning Xia <nxiamail@gmail.com>
2019-09-02 11:28:58 -0500

    Print Realm file path

--- Toodle/AppDelegate.swift
+++ Toodle/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         // Realm initialization
-        print(Realm.Configuration.defaultConfiguration.fileURL!)
+        print("Realm file", Realm.Configuration.defaultConfiguration.fileURL!)
         do {
             _ = try Realm()
         } catch {
